86bdd49f84b2b32311e44adebf1a9042bb07694d
arian ashk <akadkhoda@configit.com>
2021-04-09 17:59:53 +0200

    initial project

---
+++ Migrator/Options.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Migrator
+{
+    public class Options
+    {
+        /// <remarks>
+        ///     order of the parameters must be the same as order of properties in this class.
+        /// </remarks>
+        public Options(string path, string password, string newPassword)
+        {
+            Path = path;
+            Password = password;
+            NewPassword = newPassword;
+        }
+
+        [Option('d', "databasePath",
+            HelpText = "Path to the System.Data.SQLite encrypted database.")]
+        public string Path { get; }
+
+        [Option('p', "password",
+            HelpText = "Password of the System.Data.SQLite encrypted database.")]
+        public string Password { get; }
+
+        [Option('n', "newPassword",
+            HelpText = "Optional new password that will be used for the new SqlCipher encrypted database.")]
+        public string NewPassword { get; }
+    }
+}
---
+++ Migrator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using CommandLine;
+using Microsoft.Data.Sqlite;
+
+namespace Migrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser(with => with.EnableDashDash = true);
+            parser.ParseArguments<Options>(args).WithParsed(options =>
+            {
+                Console.WriteLine($"Start decrypting {options.Path}");
+                DecryptSystem(new SQLiteConnectionStringBuilder { DataSource = options.Path }, options.Password);
+                Console.WriteLine($"System.Data.SQLite database is now decrypted");
+                var newPassword = string.IsNullOrWhiteSpace(options.NewPassword)
+                    ? options.Password
+                    : options.NewPassword;
+                EncryptToMicrosoft(new SqliteConnectionStringBuilder { DataSource = options.Path }, newPassword);
+                Console.WriteLine($"Database is now encrypted with SqlCipher");
+            });
+        }
+
+        private static void DecryptSystem(SQLiteConnectionStringBuilder connectionStringBuilder, string password)
+        {
+            var connection = new SQLiteConnection(connectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = $"PRAGMA key = {password};";
+            command.ExecuteNonQuery();
+
+            using var command2 = connection.CreateCommand();
+            command2.CommandText = "PRAGMA rekey = '';";
+            command2.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private static void EncryptToMicrosoft(SqliteConnectionStringBuilder connectionStringBuilder, string password)
+        {
+            var databasePath = connectionStringBuilder["Data Source"]?.ToString() ?? "";
+            var databaseName = Path.GetFileNameWithoutExtension(databasePath);
+            var connection = new SqliteConnection(connectionStringBuilder.ConnectionString);
+            var databaseTemp = $"{databaseName}_Temp.db";
+            File.Delete(databaseTemp);
+            var query =
+                @$"ATTACH DATABASE '{databaseTemp}' AS encrypted KEY '{password}'; SELECT sqlcipher_export('encrypted'); DETACH DATABASE encrypted;";
+
+            connection.Open();
+            using var cmd = new SqliteCommand(query, connection);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+
+            if (File.Exists(databasePath))
+            {
+                File.Delete(databasePath);
+            }
+
+            File.Move(databaseTemp, databasePath);
+        }
+    }
+}
